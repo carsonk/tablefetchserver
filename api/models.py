a924302d6dd0a47923502d937ea2a344045de0ec
Carson Killbreath <kill4992@kettering.edu>
2016-11-12 16:26:06 -0500

    Added sqlite3 database to gitignore.

--- api/models.py
+++ api/models.py
@@ -1,3 +1,67 @@
 from django.db import models
 
-# Create your models here.
+class TableMap(models.Model):
+    """ A map to place tables on. """
+
+    name = models.CharField(max_length=256)
+
+class Table(models.Model):
+    """ Representation of a table in the restaurant. """
+
+    num_seats = models.PositiveSmallIntegerField()
+
+    x_coord = models.IntegerField() # C-coord of corner on map.
+    y_coord = models.IntegerField() # Y-coord of corner on map.
+    width = models.IntegerField() # Width in pixels on map.
+    height = models.IntegerField() # Height in pixels on map.
+    color = models.IntegerField() # 32-bit RGB hexcolor.
+
+class Party(models.Model):
+    """ A party of people sitting at a specified table. """
+
+    table = models.OneToOneField(Table, on_delete=models.CASCADE)
+
+    size = models.PositiveSmallIntegerField()
+    time_arrived = models.DateTimeField()
+    time_seated = models.DateTimeField()
+    time_paid = models.DateTimeField()
+
+class PartyMember(models.Model):
+    """ An individual member of a party. """
+
+    party = models.ForeignKey(Party, on_delete=models.CASCADE)
+
+class MenuCategory(models.Model):
+    """ A category for menu items. """
+
+    parent = models.ForeignKey("self", on_delete=models.CASCADE)
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+
+class MenuItem(models.Model):
+    """ A choosable menu item. """
+
+    category = models.ManyToManyField(MenuCategory)
+
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+
+class Order(models.Model):
+    """ Representation of an order.
+    Should be attached to party or party_member. Interface provides
+    flexibility to choose to attach order to a single person, or to a
+    party as a whole.
+    """
+
+    party = models.ForeignKey(Party, on_delete=models.CASCADE)
+    party_member = models.OneToOneField(PartyMember, on_delete=models.CASCADE)
+
+    menu_items = models.ManyToManyField(MenuItem, through='OrderMenuItem')
+
+class OrderMenuItem(models.Model):
+    """ Through model combining order and menu item. """
+
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    menu_item = models.ForeignKey(MenuItem)
+
+    comments = models.TextField()
--- tablefetchserver/settings.py
+++ tablefetchserver/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'api.apps.ApiConfig',
+    'kitchen.apps.KitchenConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

3c2eec405e279601673c9f40e772fa5865edd34f
Carson Killbreath <kill4992@kettering.edu>
2017-03-29 17:45:33 -0400

    Update permission classes on API and expose all models

--- api/urls.py
+++ api/urls.py
@@ -4,3 +4,10 @@ from api import views
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
+router.register(r'table/map', views.TableMapViewSet)
+router.register(r'tables', views.TableViewSet)
+router.register(r'parties', views.PartyViewSet)
+router.register(r'menu/categories', views.MenuCategoryViewSet)
+router.register(r'menu/items', views.MenuItemViewSet)
+router.register(r'orders', views.OrderViewSet)
+
--- api/views.py
+++ api/views.py
@@ -1,41 +1,52 @@
 from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+
 from api.models import *
 from api.serializers import *
-from rest_framework import viewsets
-from api.serializers import UserSerializer, GroupSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
+    permission_Classes = [permissions.IsAuthenticated]
 
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 class TableMapViewSet(viewsets.ModelViewSet):
     queryset = TableMap.objects.all()
     serializer_class = TableMapSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 class TableViewSet(viewsets.ModelViewSet):
     queryset = Table.objects.all()
     serializer_class = TableSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 class PartyViewSet(viewsets.ModelViewSet):
     queryset = Party.objects.all()
     serializer_class = PartySerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 class PartyMemberSet(viewsets.ModelViewSet):
     queryset = PartyMember.objects.all()
     serializer_class = PartyMemberSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 class MenuCategoryViewSet(viewsets.ModelViewSet):
     queryset = MenuCategory.objects.all()
     serializer_class = MenuCategorySerializer
+    permission_classes = [permissions.AllowAny]
 
 class MenuItemViewSet(viewsets.ModelViewSet):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+    permission_classes = [permissions.AllowAny]
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    permission_classes = [permissions.IsAuthenticated]
+

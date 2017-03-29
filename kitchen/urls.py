28cfbeba720b3cab0b58b84813b78f310c6ffe31
Carson Killbreath <kill4992@kettering.edu>
2017-03-28 20:34:09 -0400

    Made orders page flex-box compatible

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -2,9 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-     url('^$', views.orders, name='orders'),
+     url('^$', views.orders),
+     url('^orders/$', views.orders, name='orders'),
+     url('^orders/create/$', views.orders_create, name='orders_create'),
      url(r'^menu/(?P<category_id>\d+)?$', views.menu, name='menu'),
      url('^menu/create/$', views.menu_create, name='menu_create'),
      url('^menu/category/create/$', views.menu_category_create, name='menu_category_create'),
-     url('^orders/create/$', views.orders_create, name='orders_create'),
 ]

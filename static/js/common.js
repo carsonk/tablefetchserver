15519b3efaf86f63295378264d00aea7681570ab
Carson Killbreath <kill4992@kettering.edu>
2017-03-30 15:40:45 -0400

    Add breadcrumbs functionality to item selector on order page

--- static/js/common.js
+++ static/js/common.js
@@ -10,12 +10,9 @@ function csrfSafeMethod(method) {
 function jqueryAjaxSetup() {
     var csrfToken = getCsrfToken();
 
-    console.log("CSRF token set to " + csrfToken);
-
     $.ajaxSetup({
         beforeSend: function(xhr, settings) {
             if (csrfSafeMethod(settings.type) && !this.crossDomain) {
-                console.log("Setting csrf token to " + csrfToken);
                 xhr.setRequestHeader("X-CSRFToken", csrfToken);
             } else {
                 console.log("Not setting csrf token, because not safe method: " + settings.type);

eeecc7e32f15ba95a7a4cfd1015c7cad270634dd
Carson Killbreath <kill4992@kettering.edu>
2017-04-01 14:22:44 -0400

    Add price field to menu item

--- api/models.py
+++ api/models.py
@@ -61,6 +61,7 @@ class MenuItem(models.Model):
 
     name = models.CharField(max_length=256)
     description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2, default=0)
 
     def __str__(self):
         return self.name

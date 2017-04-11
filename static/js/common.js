58296822236141caa67206e1f8a0b6c92bb7e710
Carson Killbreath <kill4992@kettering.edu>
2017-04-11 13:49:19 -0400

    Ensure jquery uses CSRF token in all AJAX requests

--- static/js/common.js
+++ static/js/common.js
@@ -1,23 +1,7 @@
 
-function getCsrfToken() {
-    return Cookies.get('csrftoken');
-}
-
-function csrfSafeMethod(method) {
-    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-}
-
 function jqueryAjaxSetup() {
-    var csrfToken = getCsrfToken();
-
     $.ajaxSetup({
-        beforeSend: function(xhr, settings) {
-            if (csrfSafeMethod(settings.type) && !this.crossDomain) {
-                xhr.setRequestHeader("X-CSRFToken", csrfToken);
-            } else {
-                console.log("Not setting csrf token, because not safe method: " + settings.type);
-            }
-        }
+        headers: { "X-CSRFToken": Cookies.get('csrftoken') }
     });
 }
 
